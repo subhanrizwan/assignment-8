bbe332965dca8efc8b296755de3ec7aa1ac15cac
subhanrizwan0 <subhanahmed444sherazi@gmail.com>
2022-07-16 21:29:50 +0500

    java script

---
+++ 26 to 30/index.js
@@ -0,0 +1,95 @@
+// question 1
+// var num = "3.45214"
+// var res = Math.round(num)
+// var res1 = Math.floor(num)
+// var res2 = Math.ceil(num)
+// console.log(num);
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// question 2
+// var num = "-2.673"
+// var res = Math.round(num)
+// var res1 = Math.floor(num)
+// var res2 = Math.ceil(num)
+// console.log(num);
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+//question 3
+// var num =prompt("enter the absolute value" , "-4")
+// var res = Math.abs(num)
+// document.write(res)
+
+// question 4
+// console.log(Math.round(Math.random()*2));
+// console.log(Math.round(Math.random()*2));
+// document.write(Math.round(Math.random()*2)+"<br>")
+// document.write(Math.round(Math.random()*2))
+
+// question 5
+// var cion1=1
+// var cion2=2
+// var number1="head".toUpperCase()
+// var number2="tail".toUpperCase()
+// var random="random cion of velue ;".toUpperCase()
+
+
+// var num=Math.random()*1
+// var result=Math.round(num)
+
+// if(result===1){
+// document.write(cion1+"<br>")
+// document.write(random+number1+"<br>")
+// }
+
+// else{
+//     document.write(cion2+"<br>")
+//     document.write(random+number2)
+// }
+
+
+// question 6 1st easy and short method
+// var txt = "random numbers b/w 1 to 100; ".toUpperCase()
+// document.write(txt + Math.ceil(Math.random()*100))
+
+
+// question 6 sec method
+// var random = "random numbers b/w 1 to 100; ".toUpperCase()
+// var num = Math.random()*100
+// var res = Math.ceil(num)
+// document.write(random+res)
+
+// question 7
+// var weight = prompt("enter your weight")
+// alert("your weight is " + parseFloat(weight) + " kg")
+// document.write(parseFloat(weight))
+
+
+// question 8 short and easy method
+
+// var user = prompt("enter")
+// for(var i=0; i<=10; i++){
+//     if(user<=10){
+//         alert("congratulate") 
+//         break
+//     }else("please try again")
+
+// }
+
+// question 8 long method
+
+// var user =+prompt("enter the secret number")
+// var sec = [1,2,3,4,5,6,7,8,9,10]
+// flag = false
+// for( var i = 0; i<=sec.length ; i++){
+//     if(user===sec[i]){
+//         flag=true
+//         alert("congratulate".toUpperCase())
+//     }
+// }if(flag===false){
+//     alert("the secret num.. is not available")
+// }
+
